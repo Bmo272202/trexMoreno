8fb99d9a7e99b140611c2c57b175a1a00fb5f372
bmo272202 <bmoreno27ortega@gmail.com>
2024-06-26 16:57:36 -0500

    segundo commit

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const player = document.getElementById("player");
 const player2 = document.getElementById("player2");
 const cactus = document.getElementById("cactus");
-const cactus2 = document.getElementById("cactus2");
-const cactus3 = document.getElementById("cactus3");
 const background = document.getElementById("background");
 const buttonPlayStop = document.getElementById("buttonPlayStop");
 const board = document.getElementById("board");
 const restarButton = document.getElementById("restarGame");
-
+const img = document.getElementById("img");
+const img2 = document.getElementById("img2");
+let encolision = false;
 let score = 0;
 let intervalId;
+let random;
+let caso = 0;
 board.addEventListener("click", function () {
   playerJump();
 });
+
+function contador() {
+  caso++;
+  if (caso > 2) {
+    caso = 0;
+  }
+}
+
+function stopContador() {
+  caso--;
+}
+
+function changeDisplayImg() {
+  contador();
+  random = Math.floor(Math.random() * 2 * caso);
+  if (random === 0) {
+    img.style.display = "none";
+    img2.style.display = "none";
+  } else if (random === 1) {
+    img.style.display = "flex";
+    img2.style.display = "none";
+  } else if (random === 2) {
+    img2.style.display = "flex";
+    img.style.display = "flex";
+  }
+  console.log(random);
+}
+
+setInterval(changeDisplayImg, 5000);
 
 function playerJump() {
   player.classList.add("playerJump");
@@ -21,6 +52,8 @@ function playerJump() {
 player.addEventListener("animationend", function () {
   removeJump();
 });
+
+restarButton.addEventListener("click", restarGame);
 
 function removeJump() {
   player.classList.remove("playerJump");
@@ -33,10 +66,6 @@ function interval() {
   }, 1000);
 }
 
-function resumeScore() {
-  interval();
-}
-
 function stopScore() {
   clearInterval(intervalId);
 }
@@ -47,11 +76,10 @@ function resumeAnimation() {
   player.style.visibility = "visible";
   player2.style.visibility = "hidden";
   background.style.animationPlayState = "running";
-
-  resumeScore();
+  interval();
 }
 
-function resumeAnimatioRestar() {
+function resumeAnimationRestar() {
   cactus.style.animationPlayState = "running";
   player.style.animationPlayState = "running";
   player.style.visibility = "visible";
@@ -86,15 +114,13 @@ buttonPlayStop.addEventListener("click", () => {
   buttonPlayStop.classList.toggle("play");
 });
 
-restarButton.addEventListener("click", restarGame);
-
 function restarGame() {
   resetScore();
   removeJump();
-  // cactus.classList.remove("cactusMovement");
-  // void cactus.offsetWidth;
-  // cactus.classList.add("cactusMovement");
-  resumeAnimatioRestar();
+  cactus.classList.remove("cactusMovement");
+  void cactus.offsetWidth;
+  cactus.classList.add("cactusMovement");
+  resumeAnimationRestar();
 }
 
 function resetScore() {
